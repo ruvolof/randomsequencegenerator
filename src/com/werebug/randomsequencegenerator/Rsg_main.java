f616e82910e17c6e2307a02efc5703aea405475d
Francesco Ruvolo <ruvolof@gmail.com>
2012-11-24 12:14:12 +0100

    Code cleanup. Ads fixed.

--- src/com/werebug/randomsequencegenerator/Rsg_main.java
+++ src/com/werebug/randomsequencegenerator/Rsg_main.java
@@ -20,31 +20,64 @@ import android.content.Intent;
 
 public class Rsg_main extends Activity implements OnClickListener, OnCheckedChangeListener {
 	
-	private View range_layout;
-	private View manual_layout;
-	private Intent send_to = new Intent(Intent.ACTION_SEND);
+	// Layout widgets
+	private View range_layout, manual_layout, copy_send;
+	private Button create, copy, send_to;
+	private CheckBox digit, lowercase, uppercase, special;
+	private RadioGroup rg;
+	private TextView manual, length_textview, output;
+	
+	// String used to generate the random sequence
+	private final String BINARY = "01";
+	private final String HEX = "0123456789ABCDEF";
+	private final String DIGIT = "0123456789";
+	private final String LAZ = "qwertyuiopasdfghjklzxcvbnm";
+	private final String CAZ = "QWERTYUIOPASDFGHJKLZXCVBNM";
+	private final String SPECIAL = "$%&()=?@#<>_£[]*";
+	
+	// Intent to send text to other apps
+	private Intent send_to_intent = new Intent(Intent.ACTION_SEND);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rsg_main);
         
+        // Retrieving layout
         this.range_layout = (View)findViewById(R.id.class_range);
     	this.manual_layout = (View)findViewById(R.id.manual_layout);
+    	this.copy_send = (View)findViewById(R.id.copy_send);
+    	
+    	
+    	// Retrieving CheckBox
+    	this.digit = (CheckBox)findViewById(R.id.range_digit);
+		this.lowercase = (CheckBox)findViewById(R.id.range_lowercase);
+		this.uppercase = (CheckBox)findViewById(R.id.range_uppercase);
+		this.special = (CheckBox)findViewById(R.id.range_special);
+		
+		// Retrieving TextView
+		this.manual = (TextView)findViewById(R.id.manual);
+		this.length_textview = (TextView)findViewById(R.id.string_length);
+    	this.output = (TextView)findViewById(R.id.output_textview);
+
 
         // Setting Listener for buttons
-        Button create = (Button)findViewById(R.id.button_create);
-        create.setOnClickListener(this);
-        Button copy = (Button)findViewById(R.id.copy_button);
-        copy.setOnClickListener(this);
-        Button send_to = (Button)findViewById(R.id.send_button);
-        send_to.setOnClickListener(this);
+        this.create = (Button)findViewById(R.id.button_create);
+        this.create.setOnClickListener(this);
+        
+        this.copy = (Button)findViewById(R.id.copy_button);
+        this.copy.setOnClickListener(this);
+        
+        this.send_to = (Button)findViewById(R.id.send_button);
+        this.send_to.setOnClickListener(this);
         
         // Setting listener for RadioGroup
-        RadioGroup rg = (RadioGroup)findViewById(R.id.radio_group);
-        rg.setOnCheckedChangeListener(this);
+        this.rg = (RadioGroup)findViewById(R.id.radio_group);
+        this.rg.setOnCheckedChangeListener(this);
     }
     
+    // Implements OnCheckedChangeListener
+    // This functions hides and shows widget depending on situation
     public void onCheckedChanged (RadioGroup rg, int newchecked) {
     	switch (newchecked) {
     		case R.id.class_radio:
@@ -67,43 +100,37 @@ public class Rsg_main extends Activity implements OnClickListener, OnCheckedChan
     	int clicked = v.getId();
     	switch (clicked) {
     		case R.id.button_create:
-    	    	String chars = "";
-    	    	String result = "";
+    			String chars = "";
+    			String result = "";
     			
-    			RadioGroup rg = (RadioGroup)findViewById(R.id.radio_group);
-    	    	int selected = rg.getCheckedRadioButtonId();
+    	    	int selected = this.rg.getCheckedRadioButtonId();
     	    	
     	    	switch (selected) {
     	    		case R.id.binary_radio:
-    	    			chars = chars.concat("01");
+    	    			chars = chars.concat(this.BINARY);
     	    			break;
     	    			
     	    		case R.id.hex_radio:
-    	    			chars = chars.concat("0123456789ABCDEF");
+    	    			chars = chars.concat(this.HEX);
     	    			break;
     	    			
     	    		case R.id.class_radio:
-    	    			CheckBox digit = (CheckBox)findViewById(R.id.range_digit);
-    	    			CheckBox lowercase = (CheckBox)findViewById(R.id.range_lowercase);
-    	    			CheckBox uppercase = (CheckBox)findViewById(R.id.range_uppercase);
-    	    			CheckBox special = (CheckBox)findViewById(R.id.range_special);
-    	    			if (digit.isChecked()) {
-    	    				chars = chars.concat("0123456789");
+    	    			if (this.digit.isChecked()) {
+    	    				chars = chars.concat(this.DIGIT);
     	    			}
-    	    			if (lowercase.isChecked()) {
-    	    				chars = chars.concat("qwertyuiopasdfghjklzxcvbnm");
+    	    			if (this.lowercase.isChecked()) {
+    	    				chars = chars.concat(this.LAZ);
     	    			}
-    	    			if (uppercase.isChecked()) {
-    	    				chars = chars.concat("QWERTYUIOPASDFGHJKLZXCVBNM");
+    	    			if (this.uppercase.isChecked()) {
+    	    				chars = chars.concat(this.CAZ);
     	    			}
-    	    			if (special.isChecked()) {
-    	    				chars = chars.concat(getResources().getString(R.string.range_special));
+    	    			if (this.special.isChecked()) {
+    	    				chars = chars.concat(this.SPECIAL);
     	    			}
     	    			break;
     	    			
     	    		case R.id.manual_radio:
-    	    			TextView manual = (TextView)findViewById(R.id.manual);
-    	    			String chars_to_add = manual.getText().toString();
+    	    			String chars_to_add = this.manual.getText().toString();
     	    			chars = chars.concat(chars_to_add);
     	    			break;
     	    		
@@ -112,13 +139,15 @@ public class Rsg_main extends Activity implements OnClickListener, OnCheckedChan
     	    	}
     	    	
     	    	int chars_last_index = chars.length() - 1;
-    	    	View copy_send = (View)findViewById(R.id.copy_send);
     	    	
     	    	if (chars_last_index >= 0) {
-    	    		copy_send.setVisibility(View.VISIBLE);
-	    	    	TextView length_textview = (TextView)findViewById(R.id.string_length);
-	    	    	String length_as_string = length_textview.getText().toString();
+    	    		this.copy_send.setVisibility(View.VISIBLE);
+    	    		
+    	    		// Converting length to integer
+	    	    	String length_as_string = this.length_textview.getText().toString();
 	    	    	int selected_length = Integer.parseInt(length_as_string, 10);
+	    	    	
+	    	    	// Generating the string
 	    	    	for (int i = selected_length; i > 0; i--) {
 	    	    		long random = Math.round(Math.random()*chars_last_index);
 	    	    		int index = (int) random;
@@ -127,34 +156,31 @@ public class Rsg_main extends Activity implements OnClickListener, OnCheckedChan
 	    	    	}
     	    	}
     	    	else {
-    	    		copy_send.setVisibility(View.GONE);
+    	    		this.copy_send.setVisibility(View.GONE);
     	    	}
     	    	
-    	    	TextView output = (TextView)findViewById(R.id.output_textview);
     	    	output.setText(result);
     	    	break;
     	    	
     		case R.id.copy_button:
-    			TextView random_sequence = (TextView)findViewById(R.id.output_textview);
     			int sdk = Build.VERSION.SDK_INT;
     			if (sdk >= 11) {
 	    			ClipboardManager clipboard = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
-	    			ClipData clip = ClipData.newPlainText("rgs", random_sequence.getText());
+	    			ClipData clip = ClipData.newPlainText("rgs", this.output.getText());
 	    			clipboard.setPrimaryClip(clip);
     			}
     			else {
 					android.text.ClipboardManager old_cbm = (android.text.ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
-    				old_cbm.setText(random_sequence.getText());    				
+    				old_cbm.setText(this.output.getText());    				
     			}
     			Toast.makeText(this, R.string.copied_to_cb, Toast.LENGTH_SHORT).show();
     			break;
     		
     		case R.id.send_button:
-    			TextView sequence_to_send = (TextView)findViewById(R.id.output_textview);
-    			this.send_to.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-    			this.send_to.setType("text/plain");
-    			this.send_to.putExtra(Intent.EXTRA_TEXT, sequence_to_send.getText());
-    			startActivity(Intent.createChooser(send_to, getResources().getString(R.string.send_with)));
+    			this.send_to_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+    			this.send_to_intent.setType("text/plain");
+    			this.send_to_intent.putExtra(Intent.EXTRA_TEXT, this.output.getText());
+    			startActivity(Intent.createChooser(this.send_to_intent, getResources().getString(R.string.send_with)));
     			break;
     	    
     	    default:
