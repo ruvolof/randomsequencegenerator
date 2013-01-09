ab901111aea84faa1b204c5e5f765339b66f8af2
Francesco Ruvolo <ruvolof@gmail.com>
2013-01-09 23:24:18 +0100

    empty view fixed

--- src/com/werebug/randomsequencegenerator/ShowSaved.java
+++ src/com/werebug/randomsequencegenerator/ShowSaved.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,11 +15,13 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class ShowSaved extends Activity implements OnItemClickListener {
 	
 	// Layout
 	private ListView add_here;
+	private TextView empty_view;
 	
 	// String Array
 	private ArrayList<String> string = new ArrayList<String>();
@@ -36,22 +39,25 @@ public class ShowSaved extends Activity implements OnItemClickListener {
 		this.setContentView(R.layout.activity_show_saved);
 		
 		this.add_here = (ListView)findViewById(R.id.saved_list);
+		this.empty_view = (TextView)findViewById(R.id.empty_text);
 		
 		// Loading shared preferences and editor
 		this.sp = this.getSharedPreferences("saved_sequences", MODE_PRIVATE);
+		
+		// Creating the Adapter with all strings in an array
+		this.saved = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, string);
+		this.saved.setNotifyOnChange(true);
 		
 		// Retrieving all pairs in SharedPreferences sp
 		Map<String, ?> kv = sp.getAll();
 		
 		// Iterating through shared preferences
 		for(Map.Entry<String, ?> entry : kv.entrySet()) {		
-			string.add(entry.getKey());
+			saved.add(entry.getKey());
 		}
 		
-		// Creating the Adapter with all strings in an array
-		this.saved = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, string);
-		
-		// Setting Adapter to ListView
+		// Setting Adapter to ListView and empty View
+		this.add_here.setEmptyView(empty_view);
 		this.add_here.setAdapter(this.saved);
 		
 		// Setting Listener for ListView add_here
@@ -79,7 +85,6 @@ public class ShowSaved extends Activity implements OnItemClickListener {
 				
 				// Erasing entries from ListView
 				this.saved.clear();
-				this.saved.notifyDataSetChanged();
 				return true;
 				
 			default:
