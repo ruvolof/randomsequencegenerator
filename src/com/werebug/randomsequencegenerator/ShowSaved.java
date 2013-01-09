9fa9ce02ebb2a1976841c73eb1ae1ebd3a3e9bbf
Francesco Ruvolo <ruvolof@gmail.com>
2013-01-09 22:52:21 +0100

    delete all added to showsaved

--- src/com/werebug/randomsequencegenerator/ShowSaved.java
+++ src/com/werebug/randomsequencegenerator/ShowSaved.java
@@ -6,23 +6,25 @@ import java.util.Map;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.SharedPreferences;
-import android.util.Log;
+import android.content.SharedPreferences.Editor;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
-public class ShowSaved extends Activity implements OnItemClickListener, OnItemLongClickListener {
+public class ShowSaved extends Activity implements OnItemClickListener {
 	
 	// Layout
 	private ListView add_here;
 	
 	// String Array
 	private ArrayList<String> string = new ArrayList<String>();
+	
+	// SharedPreferences object and editor
+	private SharedPreferences sp;
 	
 	// Array adapter for ListView add_here
 	private ArrayAdapter<String> saved;
@@ -35,8 +37,10 @@ public class ShowSaved extends Activity implements OnItemClickListener, OnItemLo
 		
 		this.add_here = (ListView)findViewById(R.id.saved_list);
 		
-		// Loading shared preferences
-		SharedPreferences sp = this.getSharedPreferences("saved_sequences", MODE_PRIVATE);
+		// Loading shared preferences and editor
+		this.sp = this.getSharedPreferences("saved_sequences", MODE_PRIVATE);
+		
+		// Retrieving all pairs in SharedPreferences sp
 		Map<String, ?> kv = sp.getAll();
 		
 		// Iterating through shared preferences
@@ -44,12 +48,14 @@ public class ShowSaved extends Activity implements OnItemClickListener, OnItemLo
 			string.add(entry.getKey());
 		}
 		
+		// Creating the Adapter with all strings in an array
 		this.saved = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, string);
+		
+		// Setting Adapter to ListView
 		this.add_here.setAdapter(this.saved);
 		
 		// Setting Listener for ListView add_here
 		this.add_here.setOnItemClickListener(this);
-		this.add_here.setOnItemLongClickListener(this);
 	}
 
 	@Override
@@ -60,14 +66,25 @@ public class ShowSaved extends Activity implements OnItemClickListener, OnItemLo
 	}
 	
 	public void onItemClick (AdapterView<?> adapter, View view, int pos, long id) {
-		String s = ((TextView)view).getText().toString();
-		Log.d("clicked:", s);
+		return;
 	}
 	
-	public boolean onItemLongClick (AdapterView<?> adapter, View view, int pos, long id){
-		String s = ((TextView)view).getText().toString();
-		Log.d("long clicked:", s);
-		return true;
+	public boolean onOptionsItemSelected (MenuItem mi){
+		switch (mi.getItemId()){
+			case R.id.menu_delete_all:
+				// Erasing entries from SharedPreferences
+				Editor ed = (Editor)this.sp.edit();
+				ed.clear();
+				ed.commit();
+				
+				// Erasing entries from ListView
+				this.saved.clear();
+				this.saved.notifyDataSetChanged();
+				return true;
+				
+			default:
+				return super.onOptionsItemSelected(mi);
+		}
 	}
 
 }
